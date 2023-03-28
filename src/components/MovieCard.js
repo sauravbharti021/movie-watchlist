78f29cd35145da657435abfd1cb98270f8ef5adb
Saurav Bharti <sauravbhartibgs37@gmail.com>
2023-03-28 21:12:49 +0530

    redirects

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import MovieControls from './MovieControls';
 import overlay from '../images/overlay-alt.jpg'
+import { Fragment } from 'react';
 const MovieCard = ({movie, type}) => {
   return (
-    <div className="movie-card" >
-        <div className="overlay" id="overlay"></div>
-        
-          <img
-            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-            alt={`${movie.title} Poster` }
-          />
+    <Fragment>
+      <div className="movie-card" >
+          <div className="overlay" id="overlay"></div>
+          
+            <img
+              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              alt={`${movie.title} Poster` }
+            />
 
-        <MovieControls movie={movie} type={type}  />
-    </div>
+          <MovieControls movie={movie} type={type}  />
+      </div>
+      
+
+    </Fragment>
   )
 }
 export default MovieCard;
